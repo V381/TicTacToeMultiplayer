33a54e321d8a223131b85efaf3652affe72f9985
Pavle Paunovic <paavlee@yahoo.com>
2023-06-23 19:32:55 +0200

    Added multiplayer sockets support

--- app.js
+++ app.js
@@ -5,11 +5,30 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/views/index.html');
 });
 
+io.on('connection', socket => {
+    console.log('A user connected');
+    socket.on('move', moveData => {
+        console.log('Received move:', moveData);
+        socket.broadcast.emit('move', moveData);
+    });
+    socket.on('gameEnded', winner => {
+        console.log('Game ended:', winner ? `Player ${winner} wins!` : "It's a draw!");
+        io.emit('gameEnded', winner); 
+    });
+
+    socket.on('gameStarted', () => {
+        console.log('Game started');
+        io.emit('gameStarted');
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
 
 app.use(express.static(__dirname + '/public'))
 app.use(express.static(__dirname + '/public/views/css'))
@@ -18,5 +37,5 @@ app.use(express.static(__dirname + '/public/logic'))
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log('listening on *:3000');
+    console.log(`Server listening on port ${port}`);
 });
--- public/logic/start-game.mjs
+++ public/logic/start-game.mjs
@@ -1,6 +1,6 @@
 import { addXandO } from "./add-x-o-.mjs";
 import { clearCells } from "./clear-cells.mjs";
-
+const socket = io();
 export function startGame() {
     const startGameButton = document.querySelector('.start-game');
     startGameButton.addEventListener('click', () => {
@@ -8,6 +8,7 @@ export function startGame() {
         document.querySelector('.board').style.filter = 'none';
         document.querySelector('.board').style.pointerEvents = 'auto';
       });
+      socket.emit('gameStarted');
 }
 
 export function restartGame() {
@@ -18,5 +19,6 @@ export function restartGame() {
         clearCells();
         addXandO();
         startGame();
+        location.reload();
     });    
 }
