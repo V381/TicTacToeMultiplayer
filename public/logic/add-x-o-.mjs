2935b41cfdb0762630a6e6e7d71acfc26e1f9c1d
Pavle Paunovic <paavlee@yahoo.com>
2023-06-23 15:17:46 +0200

    Logic implemented

--- public/logic/add-x-o-.mjs
+++ public/logic/add-x-o-.mjs
@@ -1,13 +1,24 @@
+import { checkWin, checkDraw } from "./winner-logic.mjs";
+
 export function addXandO() {
     const cells = document.querySelectorAll('.cell');
+    let gameEnded = false;
     let currentPlayer = 'X';
     cells.forEach(cell => {
         cell.addEventListener('click', () => {
-        if (!cell.textContent) {
+            if (!cell.textContent && !gameEnded) {
             cell.textContent = currentPlayer;
             cell.classList.add(currentPlayer);
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-        }
+            if (checkWin(currentPlayer, cells)) {
+                gameEnded = true;
+                alert(`Player ${currentPlayer} wins!`);
+            } else if (checkDraw(cells)) {
+                gameEnded = true;
+                alert("It's a draw!");
+            } else {
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            }
+            }
         });
     });
 }
