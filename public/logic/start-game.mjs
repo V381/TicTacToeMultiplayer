f3791bbfb08ba3ae6957ef4e23141215ca57098e
Pavle Paunovic <paavlee@yahoo.com>
2023-06-23 16:06:46 +0200

    Restart game implemented

--- public/logic/start-game.mjs
+++ public/logic/start-game.mjs
@@ -1,3 +1,6 @@
+import { addXandO } from "./add-x-o-.mjs";
+import { clearCells } from "./clear-cells.mjs";
+
 export function startGame() {
     const startGameButton = document.querySelector('.start-game');
     startGameButton.addEventListener('click', () => {
@@ -5,4 +8,15 @@ export function startGame() {
         document.querySelector('.board').style.filter = 'none';
         document.querySelector('.board').style.pointerEvents = 'auto';
       });
+}
+
+export function restartGame() {
+    const startGameButton = document.querySelector('.start-game');
+    startGameButton.textContent = "Restart game?";
+    startGameButton.style.display = "block";
+    startGameButton.addEventListener("click", () => {
+        clearCells();
+        addXandO();
+        startGame();
+    });    
 }
