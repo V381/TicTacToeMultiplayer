69b62240347adfda9e9fffe38459e19637470678
Pavle Paunovic <paavlee@yahoo.com>
2023-06-27 19:51:55 +0200

    Implemented lobby

--- public/logic/start-game.mjs
+++ public/logic/start-game.mjs
@@ -1,23 +1,50 @@
 import { addXandO } from "./add-x-o-.mjs";
-import { clearCells } from "./clear-cells.mjs";
 const socket = io();
-export function startGame() {
-    const startGameButton = document.querySelector('.start-game');
-    startGameButton.addEventListener('click', () => {
-        startGameButton.style.display = 'none';
-        document.querySelector('.board').style.filter = 'none';
-        document.querySelector('.board').style.pointerEvents = 'auto';
-      });
-      socket.emit('gameStarted');
-}
 
-export function restartGame() {
-    const startGameButton = document.querySelector('.start-game');
-    startGameButton.textContent = "Restart game?";
-    startGameButton.style.display = "block";
-    startGameButton.addEventListener("click", () => {
-        clearCells();
-        addXandO();
-        startGame();
-    });    
+export function startGame() {
+  const startGameButton = document.querySelector('.start-game');
+  const board = document.querySelector('.board');
+  const pageContent = document.querySelector('.page-content');
+  let playersJoined = 0;
+  let gameStarted = false;
+
+  pageContent.classList.add('blur-effect');
+
+  startGameButton.addEventListener('click', () => {
+    startGameButton.style.display = 'none';
+    board.style.filter = 'none';
+    board.style.pointerEvents = 'auto';
+    socket.emit('gameStarted'); // Emit gameStarted event
+  });
+
+  // Listen for playerJoined event
+  socket.on('playerJoined', (players) => {
+    console.log('Players:', players);
+    playersJoined = players.length;
+
+    if (playersJoined > 4) {
+      // Display the message only to third or more users
+      if (!gameStarted) {
+        pageContent.innerHTML = 'Game is already being played. Please come back later.';
+        pageContent.classList.remove('blur-effect');
+      }
+      return;
+    }
+    if (playersJoined === 3 && !gameStarted) {
+      pageContent.classList.remove('blur-effect');
+      gameStarted = true; // Set gameStarted flag to true
+    }
+
+  });
+
+  socket.on('startGame', () => {
+    console.log('Starting the game...');
+    addXandO();
+  });
+
+  // Listen for gameAlreadyStarted event
+  socket.on('gameAlreadyStarted', () => {
+    pageContent.innerHTML = 'Game is already being played. Please come back later.';
+    pageContent.classList.add('blur-effect');
+  });
 }
